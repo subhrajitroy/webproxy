b07e359433fb9a7a09468a64fa1064beb7bcd101
subhrajit <roy.subhrajit@gmail.com>
2014-12-31 17:42:10 +0000

    Initial code to work as proxy as hard coded google

--- proxy.rb
+++ proxy.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
-require_relative 'object_ext'
+require 'json'
+require 'httpclient'
+
+set :server, 'webrick'
 
 get %r{/.*} do
-  "URL : #{request.fullpath}"
+   client = HTTPClient.new
+   host = "google.co.uk"
+   url = "http://#{host}#{request.fullpath}"
+   puts url
+   client.get_content url
 end
