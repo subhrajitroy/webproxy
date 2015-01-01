cdec31d85ad557f741bf95599904a45caf764b9e
subhrajit <roy.subhrajit@gmail.com>
2015-01-01 15:25:02 +0000

    Basic proxying for HTTP Get works

--- proxy.rb
+++ proxy.rb
@@ -4,10 +4,14 @@ require 'httpclient'
 
 set :server, 'webrick'
 
-get %r{/.*} do
+host_map = {"google" => "google.co.uk","bbc" => "bbc.co.uk","stackoverflow" => "stackoverflow.com"}
+
+get %r{/proxy/([^/]+)(/?)(.*)} do
+   host_key = params[:captures].first
+   host = host_map[host_key]
+   proxied_url = params[:captures][2]
    client = HTTPClient.new
-   host = "google.co.uk"
-   url = "http://#{host}#{request.fullpath}"
+   url = "http://#{host}/#{proxied_url}"
    puts url
    client.get_content url
 end
---
+++ request_store.rb
@@ -0,0 +1,4 @@
+class RequestStore
+
+	
+end
