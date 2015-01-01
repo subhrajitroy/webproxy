350debf5471ca6f2e4ef9528c97136c4cf7c75e2
subhrajit <roy.subhrajit@gmail.com>
2015-01-01 16:19:09 +0000

    captured get requests can be seen at /requests

--- proxy.rb
+++ proxy.rb
@@ -1,10 +1,13 @@
 require 'sinatra'
 require 'json'
 require 'httpclient'
+require_relative	'request_store'
 
 set :server, 'webrick'
 
 host_map = {"google" => "google.co.uk","bbc" => "bbc.co.uk","stackoverflow" => "stackoverflow.com"}
+
+request_store = RequestStore.new
 
 get %r{/proxy/([^/]+)(/?)(.*)} do
    host_key = params[:captures].first
@@ -13,5 +16,10 @@ get %r{/proxy/([^/]+)(/?)(.*)} do
    client = HTTPClient.new
    url = "http://#{host}/#{proxied_url}"
    puts url
+   request_store.add request.env.to_json
    client.get_content url
+end
+
+get "/requests" do
+ JSON.pretty_generate request_store.all
 end
--- request_store.rb
+++ request_store.rb
@@ -1,4 +1,15 @@
 class RequestStore
 
-	
+  def initialize
+  	@list = Array.new
+  end
+
+  def add request
+  	@list.push request
+  end
+
+  def all
+  	@list
+  end
+
 end
